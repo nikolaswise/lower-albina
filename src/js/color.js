f09103b1d2077933a2b812909f44cf1f1870ae43
Nikolas Wise <nikolaswise@gmail.com>
2015-04-21 16:01:58 -0700

    IT WORKS OMG

--- src/js/color.js
+++ src/js/color.js
@@ -1,5 +1,18 @@
 var chroma = require( "chroma-js" );
 
+var colorConfig = {
+  paleYellow:  chroma([92, 0, 31], 'lab'),
+  lightOrange: chroma([77, 22, 61], 'lab'),
+  darkOrange:  chroma([40, 39, 43], 'lab'),
+  lightGreen:  chroma([78, -28, 10], 'lab'),
+  darkGreen:   chroma([48, -25, 9], 'lab'),
+  lightBlue:   chroma([97, -2, -15], 'lab'),
+  darkBlue:    chroma([7, -2, -15], 'lab'),
+  white:       chroma([0, 0, 100], 'hsl'),
+  black:       chroma([0, 0, 0],   'hsl')
+};
+
+
 class Color {
   constructor(config) {
     this.palette = config;
@@ -15,4 +28,5 @@ class Color {
   }
 }
 
-export default Color;
+var color = new Color(colorConfig)
+export default color;
--- src/js/index.js
+++ src/js/index.js
@@ -1,51 +1,21 @@
-var chroma = require( "chroma-js" )
-var VectorTile = require('vector-tile').VectorTile;
-
-console.log(VectorTile)
-
 // Build the map
 var map = L.map('map', {
   scrollWheelZoom: false,
   doubleClickZoom: false,
   zoomControl: false,
   center: [45.539, -122.671],
-  zoom: 16,
-
+  zoom: 16
 })
 
-// Map Palette
-import Color from './color.js';
+var baseLayer = L.tileLayer('http://{s}.ashbu.cartocdn.com/nikolaswise/api/v1/map/86e05ccc48f4eb1bc6e5b11531870916:1429636411308.12/{z}/{x}/{y}.png')
+baseLayer.addTo(map);
 
-var colorConfig = {
-  paleYellow:  chroma([92, 0, 31], 'lab'),
-  lightOrange: chroma([77, 22, 61], 'lab'),
-  darkOrange:  chroma([40, 39, 43], 'lab'),
-  lightGreen:  chroma([78, -28, 10], 'lab'),
-  darkGreen:   chroma([48, -25, 9], 'lab'),
-  lightBlue:   chroma([97, -2, -15], 'lab'),
-  darkBlue:    chroma([7, -2, -15], 'lab'),
-  white:       chroma([0, 0, 100], 'hsl'),
-  black:       chroma([0, 0, 0],   'hsl')
-};
+import Zoning from './layers/zoning.js'
+Zoning.addTo(map)
 
-var color = new Color(colorConfig)
-export default color
+var referenceLayer = L.tileLayer('http://{s}.ashbu.cartocdn.com/nikolaswise/api/v1/map/f37074a8ebf44b62f5528a77731b6394:1429640791228.1301/{z}/{x}/{y}.png')
+referenceLayer.addTo(map);
 
-// Map layers
-
-L.tileLayer('https://{s}.tiles.mapbox.com/v4/nikolaswise.65959181/{z}/{x}/{y}.png?access_token=pk.eyJ1Ijoibmlrb2xhc3dpc2UiLCJhIjoieVJJcE1QUSJ9.f8co32wYW_YTeh_KM6PGLA#15').addTo(map);
-
-// import Zoning from './layers/zoning.js';
-// Zoning.addTo(map);
-
-// import River from './layers/river.js';
-// import Streets from './layers/streets.js';
-// var overlay = L.layerGroup([River, Streets])
-// overplay.addTo(ma)
-
-// import Footprints from './layers/footprints.js';
-// var footprints = L.layerGroup(Footprints)
-
-// var webmap = L.layerGroup([zoning, footprints])
-
-// webmap.addTo(map);
+var referencePane = map._createPane('leaflet-reference-pane', map.getPanes().mapPane);
+referencePane.appendChild(referenceLayer.getContainer());
+referenceLayer.setZIndex(9);
