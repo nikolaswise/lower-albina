d4d0e2801b87d9dd8d544bdef779902e856030df
Nikolas Wise <nikolaswise@gmail.com>
2015-04-22 09:43:07 -0700

    tiny hapi static server

---
+++ server.js
@@ -0,0 +1,21 @@
+// Init Hapi
+var Hapi = require('hapi')
+
+// Create a server with a host and port
+var server = new Hapi.Server()
+
+server.connection({
+    port: process.env.PORT || 8080
+});
+
+// Add the route
+server.route({
+  method: 'GET',
+  path:'/{params*}',
+  handler: {
+    directory: {
+      path: './',
+      index: true
+    }
+  }
+});
