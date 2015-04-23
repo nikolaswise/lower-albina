077763791355295df8ab2f8146c2a9e1a16f4fa0
Nikolas Wise <nikolaswise@gmail.com>
2015-04-23 09:08:06 -0700

    add footprints

--- src/js/index.js
+++ src/js/index.js
@@ -3,6 +3,7 @@ var map = L.map('map', {
   scrollWheelZoom: false,
   doubleClickZoom: false,
   zoomControl: false,
+  dragging: false,
   center: [45.539, -122.671],
   zoom: 16
 })
@@ -13,6 +14,9 @@ baseLayer.addTo(map);
 import Zoning from './layers/zoning.js'
 Zoning.addTo(map)
 
+import Footprints from './layers/footprints.js'
+Footprints.addTo(map)
+
 var referenceLayer = L.tileLayer('http://{s}.ashbu.cartocdn.com/nikolaswise/api/v1/map/1058662162db119fc8bc42aa1d3ef080:1429741486907.6099/{z}/{x}/{y}.png')
 referenceLayer.addTo(map);
 
@@ -20,7 +24,8 @@ var referencePane = map._createPane('leaflet-reference-pane', map.getPanes().map
 referencePane.appendChild(referenceLayer.getContainer());
 referenceLayer.setZIndex(9);
 
-
+//http://{s}.ashbu.cartocdn.com/nikolaswise/api/v1/map/1058662162db119fc8bc42aa1d3ef080:1429741486907.6099/{z}/{x}/{y}.png
+//  http://a.ashbu.cartocdn.com/nikolaswise/api/v1/map/1058662162db119fc8bc42aa1d3ef080:1429741486907.6099/16/10436/23434.png
 
 
 // cartodb.createLayer(map, 'https://nikolaswise.cartodb.com/api/v2/viz/a98e1bc6-e93e-11e4-921b-0e4fddd5de28/viz.json')
--- src/js/layers/footprints.js
+++ src/js/layers/footprints.js
@@ -1,5 +1,5 @@
 import addLayer from '../add-layer.js';
-import color from '../index.js';
+import color from '../color.js'
 
 var url = 'http://services.arcgis.com/rOo16HdIMeOBI4Mb/ArcGIS/rest/services/Eliot%20Building%20Footprints/FeatureServer/0';
 
