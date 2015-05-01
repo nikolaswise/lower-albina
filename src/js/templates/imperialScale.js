cd6bb2cfe4b93c0514cc5c5c4109b6e69e11c58c
Nikolas Wise <nikolaswise@gmail.com>
2015-05-01 08:28:26 -0700

    scale is lookin good

--- src/js/templates/imperialScale.js
+++ src/js/templates/imperialScale.js
@@ -1,21 +1,21 @@
 let imperialTemplate = `
   <div class="scale right" style="width:{{scale.pixelFrom.halfMile}}px">
     <h6 class="scale-unit">miles</h6>
-    <div class="scale">
-      <div class="eighth scale-section"><span class="scale-fill"></span></div>
-      <div class="eighth scale-section"><span class="scale-fill"></span></div>
-      <div class="half scale-section"><span class="scale-fill"></span></div>
+    <div class="scale-bars">
+      <div class="quarter-scale scale-section"><span class="scale-fill"></span></div>
+      <div class="quarter-scale scale-section"><span class="scale-fill"></span></div>
+      <div class="half-scale scale-section"><span class="scale-fill"></span></div>
     </div>
     <div class="scale-labels">
-      <div class="eighth text-left">
-        <h6>0</h6>
+      <div class="quarter-scale text-left">
+        <h6 class="label">0</h6>
       </div>
-      <div class="eighth text-left">
-        <h6>1/8</h6>
+      <div class="quarter-scale text-left">
+        <h6 class="label">1/8</h6>
       </div>
-      <div class="half">
-        <h6 class="left">1/4</h6>
-        <h6 class="right">1/2</h6>
+      <div class="half-scale">
+        <h6 class="label left">1/4</h6>
+        <h6 class="label right">1/2</h6>
       </div>
     </div>
   </div>
